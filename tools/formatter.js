bf6380c99351ca639768b352806db142e191753d
Vitor Espindola <vitor.espindola@byne.com.br>
2019-03-24 09:32:41 -0300

    Add inline edit option to formatter tool

--- tools/formatter.js
+++ tools/formatter.js
@@ -1,10 +1,41 @@
 #!/usr/bin/env node
-var args = process.argv.splice( /node$/.test(process.argv[0]) ? 2 : 1 );
-console.log(
-    JSON.stringify(
-        JSON.parse(
-            require('fs') .readFileSync(
-                args.length > 0 ? args[0] : "/dev/stdin"
-            )
-        ), null, 4)
-);
+
+const fs = require('fs');
+
+const usage = function() {
+    const usageText = `
+Usage: formatter.js [-i] schema.json
+
+Options:
+    -i        Edit file in-place
+  `
+
+    console.log(usageText)
+}
+
+var inline = false;
+var filename = null;
+
+const args = process.argv.splice(/node$/.test(process.argv[0]) ? 2 : 1);
+if (args.length == 1) {
+    filename = args[0];
+} else if (args.length == 2 && args[0] == '-i') {
+    inline = true;
+    filename = args[1];
+} else {
+    usage();
+    process.exit(1);
+}
+
+var formatted = JSON.stringify(
+    JSON.parse(
+        fs.readFileSync(filename)
+    ), null, 4);
+
+if (inline) {
+    fs.renameSync(filename, filename + '.bak');
+    console.log('Backp saved: ' + filename + '.bak');
+    fs.writeFileSync(filename, formatted);
+} else {
+    process.stdout.write(formatted);
+}
